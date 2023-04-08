5e57a194a33486cfc9e37c7183b26378599a49e2
Ryan <salvat36@gmail.com>
2023-04-07 23:13:57 -0500

    Passed Lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() + 1);
+
+let num3 = 10;
+let num4 = 44;
+let mod = num4 % num3;
+
+let max = Math.max(5, 10, 15, 20);
